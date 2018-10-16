2b8dba74080799281f49a42119c711a23adc9f20
Guillermo Suárez <g.suarez.asencio@me.com>
2018-10-16 18:11:28 +0200

    Initial Commit

---
+++ TimeBoard WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TimeBoard WatchKit Extension
+//
+//  Created by Guillermo Suárez Asencio on 16/10/2018.
+//  Copyright © 2018 Guillermo Suárez Asencio. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ TimeBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeBoard
+//
+//  Created by Guillermo Suárez Asencio on 16/10/2018.
+//  Copyright © 2018 Guillermo Suárez Asencio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
